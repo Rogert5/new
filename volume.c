b99a58ca6a2bc1d51b39c1f21daa281d8cabd5c4
Rogert5 <Rogert5@users.noreply.github.com>
2022-07-16 02:10:58 +0000

    automated commit by check50 [check50=True]

--- volume.c
+++ volume.c
@@ -40,7 +40,7 @@ int main(int argc, char *argv[])
 
 
     // TODO: Read samples from input file and write updated data to output file
-    uint16_t buffer;
+    int16_t buffer;
     while(fread(&buffer, sizeof(uint16_t), 1, input))
     {
         buffer *= factor;
