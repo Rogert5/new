256094cd318234e5f3f8f7afc9247aaec1cc53f4
Rogert5 <Rogert5@users.noreply.github.com>
2022-07-17 20:28:54 +0000

    automated commit by submit50

--- recover.c
+++ recover.c
@@ -16,7 +16,7 @@ int main(int argc, char *argv[])
     FILE *input_file = fopen(argv[1], "r");
 
     //check that the input_file is valid
-    if(input_file == NULL)
+    if (input_file == NULL)
     {
         printf("Could not open file");
         return 2;
@@ -50,11 +50,11 @@ int main(int argc, char *argv[])
             //Count number of images found
             count_image++;
         }
-            //check if ouput has been used for valid input
-            if (output_file != NULL)
-            {
-                fwrite(buffer, sizeof(char), 512, output_file);
-            }
+        //check if ouput has been used for valid input
+        if (output_file != NULL)
+        {
+            fwrite(buffer, sizeof(char), 512, output_file);
+        }
     }
     free(filename);
     fclose(output_file);
