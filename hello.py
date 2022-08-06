01a2f674768c385853911e494454268314f7735d
Rogert5 <Rogert5@users.noreply.github.com>
2022-08-06 04:10:51 +0000

    automated commit by check50 [check50=True]

--- hello.py
+++ hello.py
@@ -1,3 +1,4 @@
-
+# input through asking WIYN? and puting it in the name value
 name = input("what is your name?")
-print("hello,  " , name)
+# apprearently no \n remember this
+print("hello,  ", name)
