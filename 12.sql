562cbc1faa2ea0f46f481fd30599185a0c11be29
Rogert5 <Rogert5@users.noreply.github.com>
2022-10-28 19:56:05 +0000

    automated commit by check50 [check50=True]

--- 12.sql
+++ 12.sql
@@ -6,4 +6,4 @@ AND title IN
 (SELECT title FROM movies
 JOIN stars ON stars.movie_id = movies.id
 JOIN people ON people.id = stars.person_id
-WHERE people.name = "Helena Bonham Carter")
+WHERE people.name = "Helena Bonham Carter");
